994df8b21651ab547a23f79fb2a869febcc315df
charlesoller <charlesrello@gmail.com>
2023-09-23 12:25:43 -0400

    DONE

--- problems/recursive-debug.js
+++ problems/recursive-debug.js
@@ -14,8 +14,13 @@ doForAll(["a", "b", "c"], (x) => x.toUpperCase()); // => ["A", "B", "C"]
 ***********************************************************************/
 
 function doForAll(arr, action) {
+  if(arr.length === 0) return arr;
   return [action(arr[0]), ...doForAll(arr.slice(1), action)];
 }
 
+
+console.log(doForAll([], (x) => x * 2)); // => []
+console.log(doForAll([1, 2, 3], (x) => x + 1)); // => [2, 3, 4]
+console.log(doForAll(["a", "b", "c"], (x) => x.toUpperCase())); // => ["A", "B", "C"]console.log(
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 module.exports = doForAll;
